abc60996c085319c9d9e207eb79f0a9c2c28ed84
sangwon97 <osw0788@gmail.com>
2023-04-11 12:49:42 +0900

    230411 : fix divide func

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int add(int a, int b) {return a+b;}
 int minus(int a, int b ) {return a-b;}
 int multiply(int a, int b ) {return a*b;}
-int divide(int a, int b ) {return a/b;}
+float divide(int a, int b ) {return a/(float)b;}
 
 int main(void)
 {
